1a96def3f8168c41d823e3b5a261373f74a87c04
thegamerbr1 <xthegamerbr1x@gmail.com>
2018-07-22 20:22:11 -0300

    Add long_description to setup.py, for pypi readme

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='LDScriptures',
       version='0.2.0    ',
       description='Powerful tool for getting the LDS (mormon) scriptures in your python script.',
@@ -9,6 +13,7 @@ setup(name='LDScriptures',
       packages=['ldscriptures'],
       include_package_data=True,
       package_data={'ldscriptures': ['ldscriptures/languages.json']},
+      long_description=read('README.rst'),
       install_requires = ['bs4', 'requests'], 
       keywords = ['mormon', 'lds', 'latter', 'day', 'saints', 'book of mormon', 'scriptures', 'bible', 'pearl of great price',
                   'doctrine and convenants', 'church of jesus christ', 'parse', 'citation']
