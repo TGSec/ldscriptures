5b284ff5e0f7a66e98acf86d8fd698f13b27ba45
thegamerbr1 <xthegamerbr1x@gmail.com>
2019-01-22 23:47:58 -0200

    Update version to 0.3.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='LDScriptures',
-      version='0.3.5',
+      version='0.3.6',
       description='Powerful tool for getting the LDS (mormon) scriptures in your python script.',
       author='CustodiSec',
       author_email='tgb1@protonmail.com',
