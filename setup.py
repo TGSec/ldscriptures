3ccf0fa09732d36e8ce88e43fc00e1d77e101bf6
thegamerbr1 <xthegamerbr1x@gmail.com>
2019-01-22 23:46:44 -0200

    Simply can't explain.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from distutils.core import setup
 import os
 
 
@@ -6,15 +6,16 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='LDScriptures',
-      version='0.3.2',
+      version='0.3.5',
       description='Powerful tool for getting the LDS (mormon) scriptures in your python script.',
       author='CustodiSec',
       author_email='tgb1@protonmail.com',
       url='https://github.com/tgsec/ldscriptures',
       packages=['ldscriptures'],
-      data_files = [('ldscriptures', ['languages.json'])],
+      package_data = {'ldscriptures': ['ldscriptures/languages.json']},
+      data_files = [['ldscriptures', ['README.rst']]],
       long_description=read('README.rst'),
-      install_requires = ['bs4', 'requests', 'autodoc', 'cachetools'], 
+      requires = ['bs4', 'requests', 'autodoc', 'cachetools'], 
       keywords = ['mormon', 'lds', 'latter', 'day', 'saints', 'book of mormon', 'scriptures', 'bible', 'pearl of great price',
                   'doctrine and convenants', 'church of jesus christ', 'parse', 'citation']
      )
